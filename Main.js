46f0c87e8b95371fabad6625e9999a50691c0b79
SanderHansen <ssh@tv2.dk>
2013-09-30 15:00:40 +0200

    Proper DOM setup and messages now appear, lacks css and stylesheet

--- Main.js
+++ Main.js
@@ -18,15 +18,22 @@ var REFRESH_RATE = 30;
 */
 
 $(function(){
-  var Level=10;
-  var FullCrate=1;
+  var Level=0;
+  var FullCrate=0;
   var TurnsThisLevel=8;
   var SpeedThisLevel=0.2;
   var CratesThisLevel=5;
   var Delta = 0;
-  Then = new Date().getTime();
+  var Then = new Date().getTime();
   var Turning = false;
   var Crates = new Array();
+  var ShowingMessage = false;
+  
+  
+  
+    $("#MessageHUD").hide();
+    $("#BlurEffect").hide();
+    $("#MessageButton").hide();
   
   
   CreateLevel = function(){
@@ -44,7 +51,7 @@ $(function(){
         Crates[i].css("position", "absolute");
         Crates[i].attr('number',i);
         Crates[i].click(function(e){
-          if (Turning==false) {
+          if (Turning==false && !ShowingMessage) {
             if (FullCrate==$(this).attr('number'))
               Win();
             else
@@ -86,6 +93,7 @@ $(function(){
   
   
   Lose = function(){
+    ShowMessage("Flot klaret, du kom til bane "+Level, "Prøv Igen");
     RestartGame();
   }
   
@@ -103,45 +111,91 @@ $(function(){
       CreateLevel();
   }
   
+  
+  /**
+  * This function shows a message, with the proper css.
+  *
+  * @param string Message
+  * The message for the user
+  *
+  * @param string ButtonMessage
+  * The text on the button
+  */
+  function ShowMessage(Message, ButtonMessage) {
+    //Tell the game it is currently showing a message to pseudo-pause it.
+    ShowingMessage = true;
+    
+    //Append the needed containers.
+    $("#MessageHUD").show();
+    $("#BlurEffect").show();
+    $("#MessageButton").show();
+    $("#MessageButton").attr('value', ButtonMessage);
+    
+    
+    Current = $("#MessageText");
+    //Apply the string to the div, scale it, and then recenter it.
+    Current.html(Message+"<br/><br/>");
+      
+    scale = Math.min(1,Math.min(PLAYGROUND_WIDTH/Current.width(),PLAYGROUND_HEIGHT/Current.height()));
+  }
+
+  /**
+  * Unshows the message, basicly, this should only be called by the Ok button.
+  */
+  
+  function UnshowMessage(Message) {
+    ShowingMessage = false;
+    
+    $("#MessageHUD").hide();
+    $("#BlurEffect").hide();
+    
+  }
+  
+  $("#MessageButton").click(function(e) { UnshowMessage() });
+  
+  
   Step = function(){
-    //Calcualte how many miliseconds passed since last frame, to get smoother animations.
-    Now = new Date().getTime();
-    Delta = Now - Then;
-    if (Turning==false && TurnsThisLevel>0)
+    if (!ShowingMessage)
     {
-      $(".CrateDiv").css('zIndex',2);
-      var CurrentCrates=[].concat(Crates);
-      var n = Math.floor(Math.random()*CurrentCrates.length);
-      Crate1 = CurrentCrates[n];
-      CurrentCrates.splice(n,1);
-      Crate2 = CurrentCrates[Math.floor(Math.random()*CurrentCrates.length)];
-      
-      
-      Crate1.Desto=Crate2.position().left;
-      Crate1.css('zIndex',3);
-      Crate2.Desto=Crate1.position().left;
-      Crate2.css('zIndex',1);
-      PathPercent=0;
-      
-      
-      Turning=true;
-    }
-    else
-    {
-      PathPercent+=Delta*(1/SpeedThisLevel)/1000
-      if (PathPercent <= 1)
-        Crate1.css({left: Crate2.Desto + (Crate1.Desto - Crate2.Desto)*PathPercent, top: 400+ Math.sin(PathPercent*Math.PI)*Math.min(Math.abs(Crate2.Desto - Crate1.Desto),500)*0.3 });
-      if (PathPercent <= 1)
-        Crate2.css({left: Crate1.Desto + (Crate2.Desto - Crate1.Desto)*PathPercent, top: 400- Math.sin(PathPercent*Math.PI)*Math.min(Math.abs(Crate2.Desto - Crate1.Desto),500)*0.3 });
+      //Calcualte how many miliseconds passed since last frame, to get smoother animations.
+      Now = new Date().getTime();
+      Delta = Now - Then;
+      if (Turning==false && TurnsThisLevel>0)
+      {
+        $(".CrateDiv").css('zIndex',2);
+        var CurrentCrates=[].concat(Crates);
+        var n = Math.floor(Math.random()*CurrentCrates.length);
+        Crate1 = CurrentCrates[n];
+        CurrentCrates.splice(n,1);
+        Crate2 = CurrentCrates[Math.floor(Math.random()*CurrentCrates.length)];
         
-        if (PathPercent >= 1){
-          Crate1.css({left: Crate1.Desto,top: 400});
-          Crate2.css({left: Crate2.Desto,top: 400});
-          Turning=false;
-          --TurnsThisLevel;
-        }
+        
+        Crate1.Desto=Crate2.position().left;
+        Crate1.css('zIndex',3);
+        Crate2.Desto=Crate1.position().left;
+        Crate2.css('zIndex',1);
+        PathPercent=0;
+        
+        
+        Turning=true;
+      }
+      else
+      {
+        PathPercent+=Delta*(1/SpeedThisLevel)/1000
+        if (PathPercent <= 1)
+          Crate1.css({left: Crate2.Desto + (Crate1.Desto - Crate2.Desto)*PathPercent, top: 400+ Math.sin(PathPercent*Math.PI)*Math.min(Math.abs(Crate2.Desto - Crate1.Desto),500)*0.3 });
+        if (PathPercent <= 1)
+          Crate2.css({left: Crate1.Desto + (Crate2.Desto - Crate1.Desto)*PathPercent, top: 400- Math.sin(PathPercent*Math.PI)*Math.min(Math.abs(Crate2.Desto - Crate1.Desto),500)*0.3 });
+          
+          if (PathPercent >= 1){
+            Crate1.css({left: Crate1.Desto,top: 400});
+            Crate2.css({left: Crate2.Desto,top: 400});
+            Turning=false;
+            --TurnsThisLevel;
+          }
+      }
+      Then = Now;
     }
-    Then = Now;
   }
   
   //Starts the main loop
